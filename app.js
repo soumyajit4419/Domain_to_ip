c41ae2d07bcf5f389edee56d4406476a4e54eeb7
soumyajit4419 <ashish454570@gmail.com>
2019-11-12 23:26:49 +0530

    added backend

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const path = require('path');
+const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const exec = require('child_process').exec;
+const { execFile } = require('child_process');
+
+app.get('/', function (req, res, next) {
+    console.log("in the index page");
+    res.sendFile(path.join(__dirname, 'view', 'index.html'));
+
+});
 
 
 
-const app = express();
+
+app.post('/getIp', function (req, res) {
+    var name = req.body.dName
+    const child = execFile('nslookup', [`${name}`], (error, stdout, stderr) => {
+        if (error) {
+            throw error;
+        }
+        else {
+            res.json(stdout)
+        }
+    });
+
+})
+
+
 app.listen(8000);
